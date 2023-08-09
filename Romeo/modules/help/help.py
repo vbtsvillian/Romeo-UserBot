3ecc378861fa99613c3a703812350b967a09af23
vbtsvillian <141561303+vbtsvillian@users.noreply.github.com>
2023-08-09 21:45:42 +0530

    Update help.py

--- Romeo/modules/help/help.py
+++ Romeo/modules/help/help.py
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "RomeoBot Plugins"
+            ac.title = "USER-𓆰ꪜ𝓲ꪶꪶ𝓲ꪖꪀ𓆪"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @RomeoBot_op × @Romeo_op •",
+                f"```{str(ac)}```\n• @villen_012 × @HLV_community •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "😎 @Romeo_op 😎"
+            this_command += "😎 @vilen_3r 😎"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -80,12 +80,12 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "RomeoBot Plugins"
+        ac.title = "USER-𓆰ꪜ𝓲ꪶꪶ𝓲ꪖꪀ𓆪"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @RomeoBot_op × @Romeo_op •"
+            message, f"```{str(ac)}```\n• @villen_012 × @HLV_community •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "😎 @Romeo_op 😎"
+            this_command += "😎 @vilen_3r 😎"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
